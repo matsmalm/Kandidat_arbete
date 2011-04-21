79ca7517ffb858f146d0fe9c4cc13b72c2f77850
Fredrik Baberg <fredrik.baberg@gmail.com>
2011-04-21 03:11:31 +0200

    kompilera-win med uppdateringar för att endast kompilera nödvändig kod

--- Kod/kompilera-win.sh
+++ Kod/kompilera-win.sh
@@ -1,2 +1,2 @@
-echo "Run: gcc -g -c genetic.c && gcc -g -c greedy.c && gcc -g -c tabu.c && gcc -g -c hashtab.c && gcc -g -c assignmentoptimal_impl.c && gcc -g -o NodeGen NodeGenFromFile.c genetic.o greedy.o hashtab.o assignmentoptimal_impl.o tabu.o"
-gcc -g -c genetic.c && gcc -g -c greedy.c && gcc -g -c tabu.c && gcc -g -c hashtab.c && gcc -g -c assignmentoptimal_impl.c && gcc -g -o NodeGen NodeGenFromFile.c genetic.o greedy.o hashtab.o assignmentoptimal_impl.o tabu.o
+echo "Run: gcc -g -c genetic.c && gcc -g -c greedy.c && gcc -g -c tabu.c && gcc -g -c hashtab.c && gcc -g -o NodeGen NodeGenFromFile.c genetic.o greedy.o hashtab.o tabu.o"
+gcc -g -c genetic.c && gcc -g -c greedy.c && gcc -g -c tabu.c && gcc -g -c hashtab.c && gcc -g -o NodeGen NodeGenFromFile.c genetic.o greedy.o hashtab.o tabu.o
